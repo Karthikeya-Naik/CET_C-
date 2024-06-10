044be54b2665a88c606d1d7aa7ef69a47dae384d
Karthikeya-Naik <166363435+Karthikeya-Naik@users.noreply.github.com>
2024-06-10 10:46:35 +0530

    Create acess_elements.cpp

---
+++ acess_elements.cpp
@@ -0,0 +1,11 @@
+//access elements(vector)
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> g1;
+    for(int i=1;i<=10;i++)
+            g1.push_back(i*10);
+    cout<<"\nat : g1.at(4)="<<g1.at(4);
+    cout<<"\nfront : g1.front()="<<g1.front();
+    cout<<"\nback : g1.back()="<<g1.back();
+}
